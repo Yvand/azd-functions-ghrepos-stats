121861c68ecd04ca6651e5de12a20b27871b0d2b
Yvan Duhamel <yvandev@outlook.fr>
2024-10-02 12:02:22 +0200

    bump deps and update deployment workflow (#16)
    * bump deps
    
    * remove k8s
    
    * Add or update the Azure App Service build and deployment workflow config
    
    * Update master_projects-statistics-funcs.yml
    
    * Update master_projects-statistics-funcs.yml
    
    * add logging
    
    * Update master_projects-statistics-funcs.yml
    
    * Update master_projects-statistics-funcs.yml
    
    * Update and rename master_projects-statistics-funcs.yml to deploy-funcs.yml
    
    * bump deps
    
    * bump deps
    
    * do not print warning messages when installing deps
    
    * work
    
    * Use managed identity (#17)
    
    * use managed identity
    
    * use managed identity
    
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * use managed identity
    
    * work
    
    * update pipeline
    
    * update workflow
    
    * update workflow
    
    * remove outdated workflow

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -1,16 +1,16 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext, output, Timer } from "@azure/functions";
+import { app, CosmosDBOutputOptions, HttpRequest, HttpResponseInit, InvocationContext, output, Timer } from "@azure/functions";
 import { GetLatestDataFromGitHub } from "../app/gitHubRepository";
-import { RepositoryDataDocument, Settings } from "../app/common";
+import { RepositoryDataDocument, safeWait, Settings } from "../app/common";
 import { GetLatestDocument as GetLatestDocumentFromCosmosDB } from "../app/cosmosData";
 
-const cosmosOutput = output.cosmosDB({
-  connection: "CosmosConnection",
-  databaseName: Settings.CosmosDatabaseId || "<Could not read app setting CosmosDatabaseId>",
-  containerName: Settings.CosmosCollectionId || "<Could not read app setting CosmosCollectionId>",
-  // containerName: "Container1",
+const cosmosOutputOptions: CosmosDBOutputOptions = {
+  connection: Settings.ConnectionPrefix, // prefix for settings: https://learn.microsoft.com/en-us/azure/azure-functions/functions-bindings-cosmosdb-v2-output#identity-based-connections
+  databaseName: Settings.CosmosDatabaseId,
+  containerName: Settings.CosmosCollectionId,
   // createIfNotExists: true,
   // partitionKey: "/DateStatCreatedYYYYMM",
-});
+}
+const cosmosOutput = output.cosmosDB(cosmosOutputOptions);
 
 const projectsConfig: string[] | undefined = Settings.Repositories?.split(";");
 
@@ -35,7 +35,15 @@ export async function GetData(request: HttpRequest, context: InvocationContext):
     return { status: 400, body: "Invalid parameters." };
   }
 
-  const cosmosDBDocument: RepositoryDataDocument = await GetLatestDocumentFromCosmosDB(projectName); // Query CosmosDB
+  context.log(`[${projectName}] Getting data...`);
+  let cosmosDBDocument: RepositoryDataDocument;
+  let error;
+  [cosmosDBDocument, error] = await safeWait(GetLatestDocumentFromCosmosDB(projectName, context)); // Query CosmosDB
+  if (error) {
+    context.error(`[${projectName}] Could not get data from Cosmos DB: ${error.message}...`);
+    return { status: 500, body: `An unexpected error has occurred` };
+  }
+
   const cosmosDBDocumentStringified = JSON.stringify(cosmosDBDocument);
   return { body: `${callback}(${cosmosDBDocumentStringified});` };
 };
@@ -58,9 +66,9 @@ export async function RefreshData(myTimer: Timer, context: InvocationContext): P
     const additionalDownloadCount: number = Number(projectConfig.split(",")[2]);
 
     context.log(`[${projectName}] Getting data...`);
-    let promises: Promise<RepositoryDataDocument>[] = new Array(2);
+    let promises: Promise<RepositoryDataDocument | undefined>[] = new Array(2);
     promises[0] = GetLatestDataFromGitHub(projectName, mainAssetName, additionalDownloadCount); // Query GitHub
-    promises[1] = GetLatestDocumentFromCosmosDB(projectName); // Query CosmosDB
+    promises[1] = GetLatestDocumentFromCosmosDB(projectName, context); // Query CosmosDB
     const [githubDocument, cosmosDBDocument] = await Promise.all(promises);
 
     if (githubDocument === undefined) {
@@ -69,17 +77,15 @@ export async function RefreshData(myTimer: Timer, context: InvocationContext): P
     }
 
     if (cosmosDBDocument === undefined) {
-      context.warn(`[${projectName}] Could not get latest document from CosmosDB, add latest CosmosDB document to output.`);
+      context.warn(`[${projectName}] Could not get latest document from CosmosDB, return latest document to CosmosDB output: ${JSON.stringify(githubDocument)}.`);
       cosmosDBDocumentsOutput.push(githubDocument);
     } else if (githubDocument.LatestReleaseDownloadCount !== cosmosDBDocument.LatestReleaseDownloadCount) {
-      context.log(`[${projectName}] Adding a document to Cosmos DB because CosmosDB.LatestReleaseDownloadCount has ${cosmosDBDocument.LatestReleaseDownloadCount} and GitHub.LatestReleaseDownloadCount has ${githubDocument.LatestReleaseDownloadCount}...`);
+      context.log(`[${projectName}] Add document to CosmosDB because CosmosDB.LatestReleaseDownloadCount has ${cosmosDBDocument.LatestReleaseDownloadCount} and GitHub.LatestReleaseDownloadCount has ${githubDocument.LatestReleaseDownloadCount}: "${JSON.stringify(cosmosDBDocumentsOutput)}"`);
       cosmosDBDocumentsOutput.push(githubDocument);
     } else {
       context.log(`[${projectName}] CosmosDB.LatestReleaseDownloadCount has ${cosmosDBDocument.LatestReleaseDownloadCount} and is up to date`);
     }
   }));
-
-  // return undefined;
   return cosmosDBDocumentsOutput;
 }
 
@@ -91,7 +97,7 @@ app.http('GetData', {
 
 app.timer('RefreshData', {
   schedule: Settings.FuncRefreshDataSchedule,
-  runOnStartup: false,
+  runOnStartup: true,
   handler: RefreshData,
   return: cosmosOutput
 });
